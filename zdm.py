df3f227d17bad26d1c701fff02600cf44a66c8f4
zuki air <fleshshell@ctemplar.com>
2022-06-15 13:10:10 +0100

    replace sxint with just sx and waypath option

--- zdm.py
+++ zdm.py
@@ -1,7 +1,6 @@
 # zuki display manager for window managers
 import os
 import argparse
-#import subprocess
 ver=0.9
 
 def listwm(wmarray):
@@ -16,22 +15,12 @@ def way(startwm, waypath):
     os.system(y)
 
 def sx(startwm, path):
+    print(f"run 'exec {xorg} sh {path}{startwm} in shell'")
     x = f"{xorg} sh {path}{startwm}"
     print(x)
     os.system(x)
 
-
-def sxstr(startwm, path):
-    print(f"run 'exec sx sh {path}{startwm} in shell'")
-    sx(startwm, path)
-
-def sxint(startwm, path, wmarray):
-    x = wmarray[startwm - 1]
-    print(f"run 'exec sx sh {path}{x} in shell'")
-    sx(x, path)
-
 def main():
-    #wmarray = os.listdir(path)
     xwmarray = []
     ywmarray = []
     ywmprint = []
@@ -69,22 +58,17 @@ def main():
         if x in ywmarray:
             way(x, waypath)
         else:
-            #sxint(val, path, wmarray)
-            sxstr(x, path)
+            sx(x, path)
     except ValueError:
         if startwm == "q":
             quit()
-        sxstr(startwm, path)
-    #sx(startwm, path)
-#try:
-#    main()
-#except KeyboardInterrupt:
-#    print("\nKEYBOARDINTERRUPT")
+        sx(startwm, path)
 def parse_arguments():
     helpmenu = f"a cli display manager using scripts in $HOME/.config/zdm/ (for xorg wm) and $HOME/.config/zdm/way/ (for wayland wm) as options"
     parser = argparse.ArgumentParser(description = helpmenu)
     parser.add_argument("-v", "--version", help = "display ver num", action="store_true")
     parser.add_argument("-i", "--inputdir", type=str, help = "input custom dir")
+    parser.add_argument("-iw", "--inputwaydir", type=str, help = "input custom wayland dir")
     parser.add_argument("-t", "--typeindicator", help = "display session type (x) for xorg (w) for wayland ", action="store_true")
     parser.add_argument("-x", "--xinit", help = "use xinit over sx", action="store_true")
     args = parser.parse_args()
@@ -105,8 +89,11 @@ def parse_arguments():
         xorg = "xinit"
     else:
         xorg = "sx"
+    if args.inputwaydir == None:
+        waypath = (f"{path}way/")
+    else:
+        waypath = args.inputwaydir
 
-    waypath = (f"{path}way/")
 
 if __name__ == "__main__":
     parse_arguments()
